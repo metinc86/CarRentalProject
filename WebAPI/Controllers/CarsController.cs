6a5307d867c6324faaf05c68bf87961e4becff5b
Metin CANIMOGLU <naughty_metin@hotmail.com>
2022-08-31 23:45:50 +0300

    31/08/2022

--- WebAPI/Controllers/CarsController.cs
+++ WebAPI/Controllers/CarsController.cs
@@ -24,12 +24,37 @@ namespace WebAPI.Controllers
             _carService = carService;
         }
 
-        [HttpGet]
+        [HttpGet("getall")]
 
-        public List<Car> Get()
+        public IActionResult GetAll()
         {
             var result = _carService.GetAll();
-            return result.Data;
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+        [HttpGet("getbyid")]
+
+        public IActionResult GetById(int id)
+        {
+            var result = _carService.GetById(id);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+        [HttpPost("add")]
+        public IActionResult Add(Car car)
+        {
+            var result = _carService.Add(car);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
         }
     }
 }
